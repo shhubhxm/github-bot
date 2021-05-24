fc5408d41f5a619c7cc53498f179eae6b175cbf7
“shubham.vyas” <vyasshubham.41@gmail.com>
2021-05-24 19:18:22 +0530

    node modules

---
+++ index.js
@@ -0,0 +1,27 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const SimpleGit = require('simple-git');
+const random = require('random');
+
+const FILE_PATH = './data.json';
+
+const makeCommit = n => {
+    if (n === 0) return SimpleGit().push();
+    const x = random.int(0, 54); //days
+    const y = random.int(0, 6); //weeks
+    const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+    const data = {
+        date: DATE
+    }
+    console.log(DATE);
+    jsonfile.writeFile(FILE_PATH, data, () => {
+        // git commit --date="monday 28th"
+        SimpleGit().add((FILE_PATH)).commit(DATE, { '--date': DATE },
+        makeCommit.bind(this, --n));
+    });
+}
+
+// makeCommit(comlumnsFrom[0]index,rowFrom[-1]index)
+// numberOfCommits 
+makeCommit(500);
+
